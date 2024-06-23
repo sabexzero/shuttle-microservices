32b2bdd5a31be1386d9bc1dcfd0b32593ab891ea
Vitaliy <vkuksa.tech@outlook.com>
2024-06-23 23:56:15 +0300

    config change

---
+++ trip-management-service/src/main/java/com/example/tripmanagementservice/configuration/KafkaConfiguration.java
@@ -0,0 +1,40 @@
+package com.example.userservice.configuration;
+
+import com.fasterxml.jackson.databind.ser.std.StringSerializer;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaConfiguration {
+    
+    @Value("${kafka.bootstrap-servers}")
+    private String bootstrapServers;
+    
+    @Bean
+    public ProducerFactory<String, String> producerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+    
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+    
+    @Bean
+    public NewTopic newTopic(){
+        return new NewTopic("testKafka", 1, (short) 1);
+    }
+}
---
+++ trip-management-service/src/main/java/com/example/tripmanagementservice/service/TripCalculationService.java
@@ -0,0 +1,2 @@
+package com.example.tripmanagementservice.service;public class TripCalculationService {
+}
